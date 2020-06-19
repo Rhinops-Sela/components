78c98bef82d64d49fa86387a3186bb82f8903ffa
iliagerman <iliagerman@gmail.com>
2020-06-19 18:03:34 +0300

    set AWS credentials

--- cluster/create.ps1
+++ cluster/create.ps1
@@ -1,6 +1,5 @@
 #!/bin/pwsh
 
-& "/home/noama/.aws/credentials.ps1"
 aws configure set aws_access_key_id $Env:AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $Env:AWS_SECRET_ACCESS_KEY
 aws configure set region $Env:AWS_DEFAULT_REGION
