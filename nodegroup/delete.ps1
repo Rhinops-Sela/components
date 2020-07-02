661b12fb6a01e2d24b1c60c6256d3280cfe2fa0e
iliagerman <iliagerman@gmail.com>
2020-07-02 12:30:30 +0300

    fixed ng delete

--- nodegroup/delete.ps1
+++ nodegroup/delete.ps1
@@ -1,15 +1,98 @@
 #!/bin/pwsh
 Using module '$PSScriptRoot/../../common/nodegroups/nodegroup.psm1'
-if ($debug -Match 'NAME'){
-    $nodeGroupName = 'generic-ng'
-}
-else {
+
+$debug='${NAME}'
+if ($debug -Match 'NAMESPACE'){
+    $nodeGroupName = 'fennec-ng'
+    $userLabelsStr = 'label1=value1;label2=value2'
+    $instanceTypes = 't3.small,t2.small'
+    $taintsToAdd = 'taint1=true:NoSchedule;taint2=true:NoSchedule'
+    $additionalARNs = 'arn:aws:iam::aws:policy/AmazonS3FullAccess;arn:aws:iam::aws:policy/AmazonWorkMailFullAccess'
+    $useSpot = 'true'
+    $spotAllocationStrategy = 'lowest-price'
+    $onDenmandInstances = 0
+} else
+{
+    $userLabelsStr = '${LABELS}'
+    $instanceTypes = '${INSTANCE_TYPES}'
+    $taintsToAdd = '${TAINTS}'
+    $additionalARNs = '${ADITONAL_ARNS}'
+    $useSpot = '${USE_SPOT}'
+    $spotAllocationStrategy = '${SPOT_ALLOCATION_STRATEGY}'
+    $onDenmandInstances = ${ON_DEMAND_INSTANCES}
     $nodeGroupName = '${NAME}'
+    $min = '${MIN}'
+    $max = '${MAX}'
+    $desired = '${DESIRED}'
 }
 
 $nodeProperties = @{
-    nodeGroupName = "$nodeGroupName"
-    workingFilePath = "$PSScriptRoot"
+    nodeGroupName = $nodeGroupName
+    workingFilePath = "$workingFolder"
+    userLabelsStr = $userLabelsStr
+    instanceTypes = "$instanceTypes"
+    taintsToAdd = $taintsToAdd
 }
-$NodeGroup = [GenericNodeGroup]::new($nodeProperties)
+
+if($useSpot -eq 'true'){
+  $nodeProperties.spotProperties = @{
+    onDemandBaseCapacity = $onDenmandInstances
+    onDemandPercentageAboveBaseCapacity = 0
+    spotAllocationStrategy = $spotAllocationStrategy
+    useSpot = $useSpot
+  }
+}
+$NodeGroup = [GenericNodeGroup]::new($nodeProperties,"$workingFolder/templates","nodegroup-template.json")
+
+
+$debug='${NAME}'
+Write-Host "PSScriptRoot: $PSScriptRoot"
+if ($debug -Match 'NAME'){
+    $userLabelsStr = 'label1=value1;label2=value2'
+    $instanceTypes = 't3.small,t2.small'
+    $taintsToAdd = 'taint1=true:NoSchedule;taint2=true:NoSchedule'
+    $additionalARNs = 'arn:aws:iam::aws:policy/AmazonS3FullAccess;arn:aws:iam::aws:policy/AmazonWorkMailFullAccess'
+    $nodeGroupName = 'generic-ng'
+    $useSpot = 'true'
+    $spotAllocationStrategy = 'lowest-price'
+    $onDenmandInstances = 0
+     $min = 0
+    $max = 1
+    $desired = 0
+}
+else {
+    $userLabelsStr = '${LABELS}'
+    $instanceTypes = '${INSTANCE_TYPES}'
+    $additionalARNs = '${ADITONAL_ARNS}'
+    $useSpot = '${USE_SPOT}'
+    $onDenmandInstances = ${ON_DEMAND_INSTANCES}
+    $spotAllocationStrategy = '${SPOT_ALLOCATION_STRATEGY}'
+    $taintsToAdd = '${TAINTS}'
+    $nodeGroupName = '${NAME}'
+    $min = ${MIN}
+    $max = ${MAX}
+    $desired = ${DESIRED}
+}
+$nodeProperties = @{
+    workingFilePath = "$PSScriptRoot"
+    spotProperties = @{
+        onDemandBaseCapacity = $onDenmandInstances
+        onDemandPercentageAboveBaseCapacity = 0
+        spotAllocationStrategy = $spotAllocationStrategy
+        useSpot = $useSpot
+    }
+    userLabelsStr = $userLabelsStr
+    instanceTypes = $instanceTypes
+    additionalARNs = $additionalARNs
+    taintsToAdd = $taintsToAdd
+}
+
+$ngFile =  (Get-Content "$PSScriptRoot/nodegroup-template.json" | Out-String | ConvertFrom-Json)
+$ngFile.nodeGroups.minSize = $min
+$ngFile.nodeGroups.maxSize = $max
+$ngFile.nodeGroups.desiredCapacity = $desired
+$ngFile | ConvertTo-Json -depth 100 | Out-File "$PSScriptRoot/nodegroup-template-execute.json"
+
+$NodeGroup = [GenericNodeGroup]::new($nodeProperties,"$PSScriptRoot","nodegroup-template-execute.json")
 $NodeGroup.DeleteNodeGroup()
+
