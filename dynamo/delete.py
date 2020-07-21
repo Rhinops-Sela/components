860f0bc54a402828a2ad060cd6abd12696c80d99
iliagerman <iliagerman@gmail.com>
2020-07-21 15:32:10 +0300

    cleanup

--- dynamo/delete.py
+++ dynamo/delete.py
@@ -1,16 +1,31 @@
+from fennec_core_dns.core_dns import CoreDNS
 import os
-from fennec_executers.helm_executer import Helm
-from fennec_execution.execution import Execution
+from fennec_executers.kubectl_executer import Kubectl
+from fennec_helpers.helper import Helper
 from fennec_nodegorup.nodegroup import Nodegroup
 
-working_folder = os.path.join(os.getcwd(), "openvpn")
-execution = Execution(working_folder)
+working_folder = os.path.join(os.getcwd(), "dynamo")
 template_path = os.path.join(
-    execution.templates_folder, "vpn-ng-template.json")
+    working_folder, "execution/templates", "dynamo-ng-template.json")
+kubectl = Kubectl(working_folder)
 
 
-openvpn_chart = Helm(working_folder, "openvpn")
-openvpn_chart.uninstall_file(os.path.join(working_folder, "prerequisites", "openvpn-pv-claim.yaml"), "openvpn")
-openvpn_chart.delete_namespace("openvpn")
-nodegroup = Nodegroup(working_folder, template_path)
-nodegroup.delete()
+values_to_replace = {
+    'DYNAMO_ENDPOINT': f'{kubectl.execution.local_parameters["NAMESPACE"]}'}
+ui_deployment_template = os.path.join(
+    kubectl.execution.templates_folder, "admin", "01.deployment.json")
+ui_deployment_template_output = os.path.join(
+    kubectl.execution.templates_folder, "admin", "01.deployment-execute.json")
+content = Helper.replace_in_file(
+    ui_deployment_template, ui_deployment_template_output, values_to_replace)
+
+kubectl.uninstall_folder(os.path.join(
+    kubectl.execution.templates_folder, "dynamodb"), "dynamodb")
+kubectl.uninstall_folder(os.path.join(
+    kubectl.execution.templates_folder, "admin"), "dynamodb")
+
+core_dns = CoreDNS(working_folder)
+admin_record  = f"{core_dns.execution.local_parameters['ADMIN_DNS_RECORD']}=dynamodb-local-admin.{core_dns.execution.local_parameters['NAMESPACE']}.svc.cluster.local"
+dynamo_record  = f"{core_dns.execution.local_parameters['DYNAMO_DNS_RECORD']}=dynamodb-local.{core_dns.execution.local_parameters['NAMESPACE']}.svc.cluster.local"
+dns_records = f"{admin_record};{dynamo_record}"
+core_dns.delete_records(dns_records=dns_records)
