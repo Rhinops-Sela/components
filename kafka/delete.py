3842f082f70889935bc351df660806e4bc6bb700
Ilia <iliag@sela.co.il>
2021-09-09 16:12:11 +0300

    added kafka

--- kafka/delete.py
+++ kafka/delete.py
@@ -4,17 +4,10 @@ from fennec_executers.helm_executer import Helm
 from fennec_execution.execution import Execution
 
 execution = Execution(os.path.dirname(__file__))
-es_url = execution.local_parameters['ES_DNS_RECORD']
+kafka_url = execution.local_parameters['KAFKA_DNS_RECORD']
 
 namespace = execution.local_parameters['NAMESPACE']
 core_dns = CoreDNS(os.path.join(os.getcwd(), "core-dns"))
-core_dns.delete_records(f"{es_url}=elasticsearch-master.{namespace}.svc.cluster.local")
-elasticsearch_chart = Helm(os.path.dirname(__file__), "elasticsearch")
-elasticsearch_chart.uninstall_chart()
-if execution.local_parameters['INSTALL_KIBANA']:
-    kibana_url = execution.local_parameters['KIBANA_DNS_RECORD']
-    core_dns.delete_records(f"{kibana_url}=kibana-kibana.{namespace}.svc.cluster.local")
-    kibana_chart = Helm(os.path.dirname(__file__), "kibana")
-    kibana_chart.uninstall_chart()
-
-
+core_dns.delete_records(f"{kafka_url}=kafka.{namespace}.svc.cluster.local")
+kafka_chart = Helm(os.path.dirname(__file__), "kafka")
+kafka_chart.uninstall_chart()
