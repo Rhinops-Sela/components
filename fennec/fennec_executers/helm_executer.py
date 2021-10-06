d57ca8371f984afb2101379067d3f6c6d637de24
Ilia <iliag@sela.co.il>
2021-10-06 14:56:08 +0300

    removed comment line

--- fennec/fennec_executers/helm_executer.py
+++ fennec/fennec_executers/helm_executer.py
@@ -23,7 +23,6 @@ class Helm(Kubectl):
         return False
 
     def install_chart(self, release_name: str, chart_url: str = "", additional_values=[], timeout = 300):
-        #verb = "upgrade" if self.installed else "install"
         verb = "upgrade --install"
         self.execution.run_command("helm repo add stable https://charts.helm.sh/stable")
         if chart_url:
